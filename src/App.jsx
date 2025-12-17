36d65cc59033a125d24b0b5697995392bb2699ef
amit20055 <amitprajapati0458@gmail.com>
2025-12-17 12:34:47 +0530

    component

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Header,{Top,Bottom} from "./Header";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+function App(){
+  return(
+    <div>
+         <h1>First Component</h1>
+         <Color/>
+         <Fruit/>
+         <Header/>
+         <Top/>
+         <Bottom/>
+        
+         
+    </div>
+    
   )
 }
 
-export default App
+function Fruit(){
+  return(
+    <h1>Apple</h1>
+  )
+}
+function Color(){
+  return(
+    <h1>Red color</h1>
+  )
+}
+export default App;
---
+++ src/Header.jsx
@@ -0,0 +1,20 @@
+ function Header(){
+    return(
+    <div>
+        <h4>Hello Amit Prjapati</h4>
+    </div>
+    )
+    
+ }
+ export function Top(){
+    return(
+        <h4>top line</h4>
+    )
+ }
+ export function Bottom(){
+    return(
+        <h4>bottom line</h4>
+    )
+ }
+ export default Header;
+ 
